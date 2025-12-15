1a277f35c44d398143c25510047214309e46ab21
serenaxu <s.serenaxu@gmail.com>
2025-12-15 15:59:43 +0100

    Primo Salvataggio

--- database/dao.py
+++ database/dao.py
@@ -5,3 +5,33 @@ class DAO:
     Implementare tutte le funzioni necessarie a interrogare il database.
     """
     # TODO
+
+    @staticmethod
+    def getRifugi():
+        cnx = DBConnect.get_connection()
+        result = []
+        cursor = cnx.cursor(dictionary=True)
+        query = "SELECT * FROM rifugiO"
+        cursor.execute(query)
+        for row in cursor:
+            rif = Rifugio(row["id"],row["nome"], row["localita"], row["altitudine"], row["capienza"], row["aperto"])
+            result.append(rif)
+        cursor.close()
+        cnx.close()
+        return result
+
+    @staticmethod
+    def getSentieri(anno):
+        cnx = DBConnect.get_connection()
+        result = []
+        cursor = cnx.cursor(dictionary=True)
+        query = """ SELECT *
+                    FROM connessione c
+                    WHERE c.anno <= %s
+                    GROUP BY c.id_rifugio1, c.id_rifugio2"""
+        cursor.execute(query, (anno,))
+        for row in cursor:
+            result.append(Sentiero(row["id"], row["id_rifugio1"], row["id_rifugio2"], row["distanza"], row["difficolta"], row["durata"], row["anno"]))
+        cursor.close()
+        cnx.close()
+        return result
--- model/model.py
+++ model/model.py
@@ -12,6 +12,21 @@ class Model:
         Il peso del grafo è dato dal prodotto "distanza * fattore_difficolta"
         """
         # TODO
+        self.lista_nodi = DAO.getRifugi()
+        self.lista_sentieri = DAO.getSentieri(year)
+
+        for sentiero in self.lista_sentieri:
+            difficolta = self.convertiDiff(sentiero)
+            self.G.add_edge(sentiero.id_rifugio1, sentiero.id_rifugio2, weight=(difficolta * float(sentiero.distanza)))
+
+    def convertiDiff(self, sentiero):
+        if sentiero.difficolta == "facile":
+            return 1
+        elif sentiero.difficolta == "media":
+            return 1.5
+        elif sentiero.difficolta == "difficile":
+            return 2
+
 
     def get_edges_weight_min_max(self):
         """
@@ -20,6 +35,13 @@ class Model:
         :return: il peso massimo degli archi nel grafo
         """
         # TODO
+
+        minimo = 1000
+        massimo = -1000
+
+
+        for edge in self.G.edges(data=True):
+
 
     def count_edges_by_threshold(self, soglia):
         """
